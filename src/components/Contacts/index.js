6e64089c2f3148ea8c046fb86ce15ae27c0e7f32
aysutoprak <moonwater99@hotmail.com>
2021-12-02 12:42:58 +0300

    filter

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -3,7 +3,20 @@ import List from "./List";
 import Form from "./Form";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Noodle",
+      phone_number: "101010",
+    },
+    {
+      fullname: "Kedi",
+      phone_number: "123123",
+    },
+    {
+      fullname: "Su",
+      phone_number: "998877",
+    },
+  ]);
 
   useEffect(() => {
     console.log(contacts);
